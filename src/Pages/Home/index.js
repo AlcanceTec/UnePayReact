9b399094c87ce5ea0a8951ab7b773d138523f1bd
Rafael Henrique Lisot <rafael.lisot@unoesc.edu.br>
2021-11-08 11:55:37 -0300

    Menu filter and styles

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -7,7 +7,25 @@ function Home() {
     return (
         <C.Container>
             <Menu/>
-            
+            <C.Content>
+                <C.Filter>
+                    <C.ItemFilter>
+                        De
+                    </C.ItemFilter>
+                    <C.ItemFilter>
+                        Até
+                    </C.ItemFilter>
+                    <C.ItemFilter>
+                        Motorista
+                    </C.ItemFilter>
+                    <C.ItemFilter>
+                        Combustivel
+                    </C.ItemFilter>
+                    <C.ApplyButton>
+                        Aplicar
+                    </C.ApplyButton>
+                </C.Filter>
+            </C.Content>
         </C.Container>
     );
 }
--- src/Pages/Home/style.js
+++ src/Pages/Home/style.js
@@ -6,5 +6,39 @@ export default {
         width: 1440px;
         height: 1420px;
         margin: auto;
+        display: flex;
     `,
+    Content: styled.div`
+
+    `,
+    Filter: styled.div`
+        background-color: #C4C4C4;
+        width: 900px;
+        height: 100px;
+        margin-top: 101px;
+        margin-left: 50px;
+        border-radius: 50px;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    `,
+    ApplyButton: styled.button`
+        width: 82px;
+        height:82px;
+        background-color: #AEAEAE;
+        border-radius: 50%;
+        margin: 9px 17px 9px 0;
+        border: none;
+    `,
+    ItemFilter: styled.div`
+        width: 160px;
+        height: 80px;
+        border-right: 1px solid #9D9D9D;
+        text-align: left;
+        margin: 0 20px 0 0;
+
+        &:nth-child(4) {
+            border-right: 0;
+        }
+    `
 }
