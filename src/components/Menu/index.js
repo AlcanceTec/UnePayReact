b9c4053d4e4b9c0100b2d7a8c3a3efd041fa1d91
NatanGtZ <natan.gabriel@uneosc.edu.br>
2021-11-09 15:29:03 -0300

    finished menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,39 +2,102 @@ import React, {useState} from 'react';
 import { ThemeProvider } from 'styled-components';
 import C from './style';
 
-export default(props) => {
+export default() => {
 
     const [open, setOpen] = useState(true);
     const [width, setWidth] = useState("200px");
     const [effect, setEffect] = useState("");
+    const [links, setLinks] = useState("flex");
+    const [logo, setLogo] = useState("48px");
+    const [rotate1, setRotate1] = useState("rotate(-45deg)");
+    const [rotate2, setRotate2] = useState("rotate(45deg)");
+    const [button, setButton] = useState("165px");
+    const [efect, setEFect] = useState("");
 
     const MenuToggle = () => {
         
         if (open === true) {
             setOpen(false);
             console.log(width);
-            setWidth("150px");
+            setWidth("120px");
             setEffect("ease-in-out 0.3s");
+            setEFect("0.2s linear");
+            setLinks("none");
+            setLogo("4px");
+            setRotate1("rotate(45deg)");
+            setRotate2("rotate(-45deg)");
+            setButton("85px");
         } else {
             setOpen(true);
             console.log(width);
             setWidth("200px");
             setEffect("ease-in-out 0.3s");
+            const timer = setTimeout(() =>{
+                setLinks("flex");
+            }, 300);
+            setLogo("48px");
+            setRotate1("rotate(-45deg)");
+            setRotate2("rotate(45deg)");
+            setButton("165px");
         }
     }
 
     return (
             <C.menuArea>
                 <C.Menu width={width} effect={effect}>
+                    <C.menuLogo logo={logo} effect={effect}>Logo</C.menuLogo>
                     <C.item>
-                        <C.itemPhoto>
-                            
-                        </C.itemPhoto>
-                        <C.itemText>
+                        <C.itemPhoto></C.itemPhoto>
+                        <C.itemText links={links}>
                             <C.links>Home</C.links>
                         </C.itemText>
                     </C.item>
-                    <C.Button onClick={MenuToggle}></C.Button>
+                    <C.item>
+                        <C.itemPhoto></C.itemPhoto>
+                        <C.itemText links={links} effect={effect}>
+                            <C.links>Home</C.links>
+                        </C.itemText>
+                    </C.item>
+                    <C.item>
+                        <C.itemPhoto></C.itemPhoto>
+                        <C.itemText links={links} effect={effect}>
+                            <C.links>Home</C.links>
+                        </C.itemText>
+                    </C.item>
+                    <C.item>
+                        <C.itemPhoto></C.itemPhoto>
+                        <C.itemText links={links} effect={effect}>
+                            <C.links>Home</C.links>
+                        </C.itemText>
+                    </C.item>
+                    <C.Button onClick={MenuToggle} button={button} efect={efect} >
+                        <C.iconButton1 rotate1={rotate1} ></C.iconButton1>
+                        <C.iconButton2 rotate2={rotate2}></C.iconButton2>
+                    </C.Button>
+                    <C.item>
+                        <C.itemPhoto></C.itemPhoto>
+                        <C.itemText links={links} effect={effect}>
+                            <C.links>Home</C.links>
+                        </C.itemText>
+                    </C.item>
+                    <C.item>
+                        <C.itemPhoto></C.itemPhoto>
+                        <C.itemText links={links} effect={effect}>
+                            <C.links>Home</C.links>
+                        </C.itemText>
+                    </C.item>
+                    <C.item>
+                        <C.itemPhoto></C.itemPhoto>
+                        <C.itemText links={links} effect={effect}>
+                            <C.links>Home</C.links>
+                        </C.itemText>
+                    </C.item>
+                    <C.item>
+                        <C.itemPhoto></C.itemPhoto>
+                        <C.itemText links={links} effect={effect}>
+                            <C.links>Home</C.links>
+                        </C.itemText>
+                    </C.item>
                 </C.Menu>
             </C.menuArea>
     )
--- src/components/Menu/style.js
+++ src/components/Menu/style.js
@@ -3,13 +3,14 @@ import styled from 'styled-components';
 export default {
     
     menuArea: styled.div`
-        width: 200px;
+        width: 400px;
         height: 1420px;
         left: 0;
         top: 0;
         border-radius: 0 20px 10px 0;
         display: flex;
         flex-direction: column;
+        z-index: 999;
     `,
 
     Menu: styled.div`
@@ -24,6 +25,17 @@ export default {
         transition: ${(props) => props.effect};
     `,
 
+    menuLogo: styled.div`
+        width: 112px;
+        height: 65px;
+        background-color: #AEAEAE;
+        margin-top: 49px;
+        margin-left: ${(props => props.logo)};
+        transition: ${(props) => props.effect};
+        text-align: center;
+        
+    `,
+
 
     Button: styled.button`
         width: 70px;
@@ -31,20 +43,44 @@ export default {
         background-color: #AEAEAE;
         border: none;
         border-radius: 50%;
+        margin: -5px 0 -50px 0;
+        margin-left: ${(props) => props.button};
+        display: flex;
+        flex-direction: column;
+        transition: ${(props) => props.efect};
+    `,
+
+    iconButton1: styled.div`
+        height: 3px;
+        width: 25px;
+        background-color: #000;
+        margin: 23px 0 0 15px;
+        transform: ${(props) => props.rotate1};
+    `,
+    
+    iconButton2: styled.div`
+        height: 3px;
+        width: 25px;
+        background-color: #000;
+        margin: 14px 0 0 15px;
+        transform: ${(props) => props.rotate2};
     `,
 
     item: styled.div`
-        width: 200px;
+        width: ${(props) => props.width};
         height: 50px;
-        background-color: #9C9A9A;
         display: flex;
+        margin-top: 30px;
+
+        &:hover {
+            background-color: #9C9A9A;
+        }
     `,
 
     itemText: styled.div`
         width: 100px;
         height: 50px;
-        text-align: center;
-        align-items: center;
+        display: ${(props) => props.links};
     `,
 
     itemPhoto: styled.div`
@@ -56,10 +92,17 @@ export default {
     `,
 
     links: styled.a`
-        font-size: 15px;
-        font-weight: bold;
+        font-size: 20px;
+        line-height: 23px;
         position: relative;
+        text-align: center;
+        margin: auto;
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: normal;
     `
+
+
 
 
 
